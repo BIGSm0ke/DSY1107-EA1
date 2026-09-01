f2e53788620cd23ec2aef92c401cc00bf435f812
AV-Alumno <AV-Alumno@duoc.acad>
2026-08-31 21:07:21 -0400

    react

---
+++ backend/src/main/java/com/DSY1107_EA1/castor/CastorApplication.java
@@ -0,0 +1,13 @@
+package com.DSY1107_EA1.castor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/DSY1107_EA1/castor/CastorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DSY1107_EA1.castor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
